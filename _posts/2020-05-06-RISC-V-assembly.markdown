d565efdff6cfa40d3fdd201dd3f7f2cde06dac68
Nao Taco <naotaco@gmail.com>
2020-05-06 22:05:23 +0900

    today

---
+++ _posts/2020-05-06-RISC-V-assembly.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title: RISC-VアセンブリでLチカする
+date: 2020-05-06 22:03:26 +09:00
+categories: Embedded RISC-V
+---
+
+急にRISC-Vに興味が出てきたので（RISC-V原典を積んだのはもうだいぶ前になるが）、
+
--- make_today.sh
+++ make_today.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 TITLE=$1
-NAME=_posts/$(date --rfc-3339 date)_${TITLE}.md
+NAME=_posts/$(date --rfc-3339 date)-${TITLE}.markdown
 touch ${NAME}
 
 echo "---" >> ${NAME}
 echo "layout: post" >> ${NAME}
 echo "title: ${TITLE}" >> ${NAME}
-DATE=$(date --rfc-3339 seconds)
+DATE=$(date --rfc-3339 seconds | sed -e 's/\+/ \+/')
 echo "date: ${DATE}" >> ${NAME}
 echo "categories: none" >> ${NAME}
 echo "---" >> ${NAME}
